0d6d07e18f534e546c2e9b12d6cbbb1410b3ab41
leeleegabriel <lgv0001@auburn.edu>
2018-11-18 12:11:01 -0600

    fixed Wiki data collection

--- data.py
+++ data.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-from time import gmtime, strftime, sleep
 from concurrent.futures import ProcessPoolExecutor
 import concurrent.futures
-import time
-import datetime
-import os
 import logging
 import asyncio
-import sqlite3
 
 from lib.GenericRSSData import RSS
 from lib.StockData import Stock
-from lib.DataScreen import Screen
+from lib.ScreenData import Screen
+from lib.WikiData import Wiki
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 fmt = logging.Formatter('%(asctime)s - %(threadName)-11s -  %(levelname)s - %(message)s')
 
 
-fh = logging.FileHandler('./data/RSS.log')
+fh = logging.FileHandler('./data/data.py.log')
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(fmt)
 logger.addHandler(fh)
@@ -34,6 +30,7 @@ Alpha_url = 'http://www.alphavantage.co/query'
 Ticker_file = './data/ticker.txt'
 RSS_DB_file = './data/data.db'
 Stock_DB_file = './data/stock.db'
+Wiki_DB_file = './data/wiki.db'
 
 Processes_Count = 4
 Fin_TIMEOUT = 5
@@ -41,32 +38,35 @@ VERSION = '0.2'
 
 
 def main():
-	Companies = [line.strip() for line in open(Ticker_file).readlines()]
-	logger.debug('Looking up RSS on %s' % str(Companies))
+	Ticker_Companies = [line.strip().split(': ')[0] for line in open(Ticker_file).readlines()]
+	Wikipedia_Companies = [line.strip().split(' : ')[1] for line in open(Ticker_file).readlines()]
+	logger.debug('Looking up RSS on %s' % str(Ticker_Companies ))
 
 	loop = asyncio.get_event_loop()
 	executor = ProcessPoolExecutor()
 
-	Yahoo = RSS(logger, RSS_DB_file, 'Yahoo', 'http://finance.yahoo.com/rss/headline?s=', Companies)
-	NASDAQ = RSS(logger, RSS_DB_file, 'NASDAQ', 'http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol=', Companies)
-	DataStock = Stock(logger, Stock_DB_file, Alpha_url, Alpha_api_key, Companies)
-	DataScreen = Screen(VERSION, Yahoo, NASDAQ, DataStock)
+	Yahoo_Data = RSS(logger, RSS_DB_file, 'Yahoo', 'http://finance.yahoo.com/rss/headline?s=', Ticker_Companies)
+	NASDAQ_Data = RSS(logger, RSS_DB_file, 'NASDAQ', 'http://articlefeeds.nasdaq.com/nasdaq/symbols?symbol=', Ticker_Companies)
+	Stock_Data = Stock(logger, Stock_DB_file, Alpha_url, Alpha_api_key, Ticker_Companies )
+	Wiki_Data = Wiki(logger, Wiki_DB_file, Wikipedia_Companies)
+	Screen_Data = Screen(VERSION, Yahoo_Data, NASDAQ_Data, Stock_Data, Wiki_Data)
 
-	future_Yahoo = loop.run_in_executor(None, Yahoo.run, loop)
-	future_NASDAQ = loop.run_in_executor(None, NASDAQ.run, loop)
-	future_Stock = loop.run_in_executor(None, DataStock.run, loop)
-	future_Screen = loop.run_in_executor(None, DataScreen.run, loop)
+	future_Yahoo = loop.run_in_executor(None, Yahoo_Data.run, loop)
+	future_NASDAQ = loop.run_in_executor(None, NASDAQ_Data.run, loop)
+	future_Stock = loop.run_in_executor(None, Stock_Data.run, loop)
+	future_Wiki = loop.run_in_executor(None, Wiki_Data.run, loop)
+	future_Screen = loop.run_in_executor(None, Screen_Data.run, loop)
 
 	try:
 		loop.run_forever()
 	except KeyboardInterrupt:
 		print('\r')
 		logger.info('Detected KeyboardInterrupt')
-		Yahoo.running = False
-		NASDAQ.running = False
-		DataStock.running = False
-		DataScreen.running = False
-	while not DataScreen.fin:
+		Yahoo_Data.running = False
+		NASDAQ_Data.running = False
+		Stock_Data.running = False
+		Screen_Data.running = False
+	while not Screen_Data.fin:
 		pass
 	loop.close()
 	logger.info('Fin.')
