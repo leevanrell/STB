47778769ef71b89de17de3285d1407ed91c729d4
leeleegabriel <leeleegabriel@gmail.com>
2019-04-23 21:40:00 -0500

    LOL BITCH

--- data/data.py
+++ data/data.py
@@ -15,31 +15,42 @@ import asyncio
 import argparse
 import os
 import time
+import codecs
+import numpy
+import csv
+from urllib.request import urlopen
 from concurrent.futures import ProcessPoolExecutor
 from elasticsearch import Elasticsearch
 
-from lib.Stock_Data import Basics, Stats
+from lib.Proxy_Basic_Data import Basics
+from lib.Stats_Data import Stats
 from lib.RSS_Data import RSS
 from lib.Upload_Data import Upload
 from lib.Wiki_Data import Wiki
-
-import conf.config
+from conf.config import *
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
+
+#logging.getLogger('requests').setLevel(logging.CRITICAL)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 fmt = logging.Formatter('%(asctime)s - %(threadName)-11s - %(levelname)s - %(message)s')
 
 sh1 = logging.StreamHandler()
-sh1.setLevel(logging.DEBUG)
+sh1.setLevel(logging.INFO)
 sh1.setFormatter(fmt)
 logger.addHandler(sh1)
 
-# fh2 = logging.FileHandler('%s/conf/logs/data.log' % (PATH))
-# fh2.setLevel(logging.INFO)
-# fh2.setFormatter(fmt)
-# logger.addHandler(fh2)
+fh2 = logging.FileHandler('%s/conf/logs/data.log' % (PATH))
+fh2.setLevel(logging.DEBUG)
+fh2.setFormatter(fmt)
+logger.addHandler(fh2)
+
+fh2 = logging.FileHandler('%s/conf/logs/error.log' % (PATH))
+fh2.setLevel(logging.ERROR)
+fh2.setFormatter(fmt)
+logger.addHandler(fh2)
 
 
 def main(args):
@@ -51,18 +62,18 @@ def main(args):
 	if args.rssd or args.statsd or args.wikid:
 		watch_list = getWatchList()
 		q = asyncio.Queue()
-		Threads = [Upload(logger, server, index, q)]
 
-		if args.rssd:
-			RSS_thread = RSS(logger, index, q, watch_list)
-			Threads.append(RSS_thread)
+		ES_thread = Upload(logger, server, 5, q)
+		Threads = [ES_thread]
 		if args.statsd:
-			Stats_thread = Stats(logger, index, q, watch_list, Alpha_API_Key)
+			Stats_thread = Stats(logger, index_name[2], q, watch_list, Alpha_API_Key)
 			Threads.append(Stats_thread) 
-			logger, DB_file, Ticker_file
 		if args.wikid:
-			Wiki_thread = Wiki(logger, index, q, watch_list)
+			Wiki_thread = Wiki(logger, index_name[3], q, watch_list)
 			Threads.append(Wiki_thread)
+		if args.rssd:
+			RSS_thread = RSS(logger, index_name[4], q, watch_list)
+			Threads.append(RSS_thread)
 
 		loop = asyncio.get_event_loop()
 		for thread in Threads:
@@ -113,16 +124,27 @@ def initSTB():
 
 	q = asyncio.Queue()
 
-	NASDAQ_thread = Basics(logger, index, q, NASDAQ_url)
-	NYSE_thread = Basics(logger, index, q, NYSE_url)
-	ES_thread = Upload(logger, server, index, q)
-	Threads = [ES_thread, NASDAQ_thread, NYSE_thread]
+	stream = urlopen(NASDAQ_url)
+	NASDAQ = list(csv.reader(codecs.iterdecode(stream, 'utf-8')))
+	del NASDAQ[0]
+
+	stream = urlopen(NYSE_url)
+	NYSE= list(csv.reader(codecs.iterdecode(stream, 'utf-8')))
+	del NYSE[0]
+
+	Companies = list(NASDAQ + NYSE)
+	Companies = list(divide_chunks(Companies, Threads_count))
+
+	ES_thread = Upload(logger, server, 5, q)
+	Threads = [ES_thread]
+	for i in range(0, Threads_count):
+		thread = Basics(logger, index_names[0], q, i, Companies[i])
+		Threads.append(thread)
 
 	loop = asyncio.get_event_loop()
-	#executor = ProcessPoolExecutor()
+	# #executor = ProcessPoolExecutor(Threads_count)
 	for thread in Threads:
 		loop.run_in_executor(None, thread.run, loop)
-
 	logger.info('Gathering Basic info on NASDAQ and NYSE companies')
 	loop.run_forever()
 	waitforThreads(Threads)
@@ -135,14 +157,22 @@ def initDB():
 	logger.info('Initializing Elasticsearch at %s' % server)
 
 	es = Elasticsearch([server])
-	if not es.indices.exists(index="stock"):
-		es.indices.create(index=index, ignore=400, body=es_mappings)
-	# else: 
-	# 	es.indices.delete(index=index, ignore=[400, 404])
-	# 	es.indices.create(index=index, ignore=400, body=es_mappings)
+
+	# es.indices.delete(index=index_names[0], ignore=404)
+	# es.indices.delete(index=index_names[1], ignore=404)
+	# es.indices.delete(index=index_names[2], ignore=404)
+	# es.indices.delete(index=index_names[3], ignore=404)
+	# es.indices.delete(index=index_names[4], ignore=404)
+	es.indices.create(index=index_names[0], ignore=400, body=es_mappings[0])
+	es.indices.create(index=index_names[1], ignore=400, body=es_mappings[1])
+	es.indices.create(index=index_names[2], ignore=400, body=es_mappings[2])
+	es.indices.create(index=index_names[3], ignore=400, body=es_mappings[3])
+	es.indices.create(index=index_names[4], ignore=400, body=es_mappings[4])
+
 
 def getWatchlist():
 	pass
+
 
 def waitforThreads(threads):
 	for thread in threads[1:]:
@@ -152,6 +182,13 @@ def waitforThreads(threads):
 	while not threads[0].Fin:
 		sleep(1)
 	logger.info('Fin.')
+
+
+def divide_chunks(l, n): 
+      
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
 
 
 if __name__ == "__main__":
